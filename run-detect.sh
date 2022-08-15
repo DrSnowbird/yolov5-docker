1ad22f9d9627f2e97ce7af2df826548cf81efadc
DrSnowbird <DrSnowbird@openkbs.org>
2022-08-15 15:47:21 -0400

    updated: run-detect.sh to handle pip install yolov5 requirements

--- run-detect.sh
+++ run-detect.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+PROJ_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+echo "#### >>>> $0: PROJ_DIR: ${PROJ_DIR}"
+
 echo "------------------------------------------------"
 echo "---- 0. CUSTOMIZED: SCRIPTS: DETECT: setup: ----"
 echo "------------------------------------------------"
@@ -9,22 +12,46 @@ if [ -s ${CUSTOMIZED_DETECT_BASH} ]; then
     ${CUSTOMIZED_DETECT_BASH} "$@"
     exit 0
 else
-    echo "... NOT FOUND: './customized/run-detect.sh' script found -- USE Demo script: './run-detect.sh' ..."
+    echo "#### >>>> NOT FOUND: './customized/run-detect.sh' script found -- USE Demo script: './run-detect.sh' ..."
 fi
 
 echo "-------------------------------------------"
 echo "---- 1) APP: GIT: Source code:  setup: ----"
 echo "-------------------------------------------"
-APP_HOME=${APP_HOME:-./app}
-if [ ! -s app ]; then
-    mdkir -p ${APP_HOME}
+APP_HOME=${APP_HOME:-${PROJ_DIR}/app}
+if [ ! -s ${APP_HOME} ]; then
+    mkdir -p ${APP_HOME}
 fi
 
-if [ ! -s app/.git ]; then
-    git clone https://github.com/ultralytics/yolov5.git ${APP_HOME}
+YOLO_GIT=${YOLO_GIT:-https://github.com/DrSnowbird/yolov5.git}
+#YOLO_GIT=${YOLO_GIT:-https://github.com/ultralytics/yolov5.git}
+if [ ! -s ${APP_HOME}/.git ]; then
+    echo -e "#### >>>>  YOLO_GIT= ${YOLO_GIT}"
+    git clone ${YOLO_GIT} ${APP_HOME}
+    pip install -r ${APP_HOME}/requirements.txt 
 fi
 
 cd ${APP_HOME}
+
+DOCKER_RUN=1
+if [ "${DOCKER_HOST_IP}" == "" ]; then
+    DOCKER_RUN=0
+fi
+
+echo
+echo "#### >>>> $0: PROJ_DIR: ${PROJ_DIR}"
+echo
+
+if [ ${DOCKER_RUN} -lt 1 ]; then
+   echo 
+   echo -e "#### >>>> Install requirements: "
+   echo
+   ls -al ${PROJ_DIR}/requirements.txt
+   echo 
+   if [ -s ${PROJ_DIR}/requirements.txt ]; then
+       pip install -r ${PROJ_DIR}/requirements.txt
+   fi
+fi
 
 echo "------------------------------------------------"
 echo "---- 1. GPU: DETECT: setup:                 ----"
